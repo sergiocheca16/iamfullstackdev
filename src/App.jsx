e988240c2e24530a68c0153021d38492bf1499db
Carlos <carlosdiazgirol@hotmail.com>
2024-04-17 10:56:57 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from './Home.jsx'
 import ItemDetailPage from "./ItemDetailPage.jsx";
+import InputCreate from "./InputCreate.jsx";
 
 const App = () => {
   const [data, setData] = useState(null)
-  const urlApi = 'http://localhost:3000'
+  const urlApi = import.meta.env.VITE_APP_API_URL
 
 const fetchData = async () => {
   try {
@@ -19,19 +20,21 @@ const fetchData = async () => {
 
 useEffect(() => {
   fetchData()
-}, [])
+}, [data])
 
   return (
     <Router>
       <div>
         <nav>
           <Link to="/">Inicio</Link>
+          <Link to="/create">create</Link>
         </nav>
         {data === null 
         ? (<div>cargando...</div>) 
         : 
           <Routes>
             <Route path="/" element={<Home data={data} />} />
+            <Route path="/create" element={<InputCreate />} />
             {data.map(item => (
               <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item}/>} />
             ))
