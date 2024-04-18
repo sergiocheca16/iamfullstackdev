a09b9e5f4c95d7c5dfa73b8816317035489c4fdf
Carlos <carlosdiazgirol@hotmail.com>
2024-04-18 09:42:16 +0200

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from './Home.jsx'
 import ItemDetailPage from "./ItemDetailPage.jsx";
-import InputCreate from "./InputCreate.jsx";
+
 
 const App = () => {
   const [data, setData] = useState(null)
@@ -20,21 +20,21 @@ const fetchData = async () => {
 
 useEffect(() => {
   fetchData()
-}, [data])
+}, [])
 
   return (
     <Router>
       <div>
         <nav>
           <Link to="/">Inicio</Link>
-          <Link to="/create">create</Link>
+     
         </nav>
         {data === null 
         ? (<div>cargando...</div>) 
         : 
           <Routes>
             <Route path="/" element={<Home data={data} />} />
-            <Route path="/create" element={<InputCreate />} />
+           
             {data.map(item => (
               <Route key={item._id} path={`/${item._id}`} element={<ItemDetailPage item={item}/>} />
             ))
